d5889dd7697e16db6d036351a568d2c2e97b5c03
Jonathan Fernandes <int3rlop3r@yahoo.in>
2021-12-16 00:13:03 +0530

    nother thing that doesn't work

--- day15.1.py
+++ day15.1.py
@@ -5,67 +5,85 @@ from queue import Queue
 # def get_val(grid, coord):
 get_val = lambda grid, coord: grid[coord[0]][coord[1]]
 
+def iter_grid(grid):
+    for r, row in enumerate(grid):
+        for c, elem in enumerate(row):
+            yield r, c, elem
+
 def get_risk_cnt(fd): 
     grid = make_grid(fd)
     # print_grid(grid)
-    q = Queue()
-    startcoord = (0, 0)
+    start = (0, 0)
+    table = {start: {'prev': (), 'cost': 0, 'currval': 1}}
     endcoord = (len(grid)-1, len(grid[0])-1)
-    # cost = get_val(grid, startcoord)
-    cost = 0 # starting position is never entered so risk isn't calculated
+    count = 0
+    for r, c, elem in iter_grid(grid):
+        curr = (r, c)
+        # if curr == start:
+            # # skip start
+            # continue
+        neighs = get_neighbors(grid, curr) # get_neighbors_reverse
+        print(curr, neighs)
+        # curr_cost = table[curr]['cost']
+        if len(neighs) == 0: # will never happen now
+            continue
 
-    q.put((startcoord, (), cost))
-    # print(endcoord)
-    table = {} # { (0, 0): {"prev": (), "cost:" get_val((0, 0))} }
-    while not q.empty():
-        coord, prev, cost = q.get()
-        print("popping", coord)
-        if coord not in table:
-            table[coord] = {'cost': cost, 'prev': prev}
+        # ncoor = selected[0] # coors are in '0'
+        for ncoor, nval in neighs:
+            if curr not in table:
+                table[curr] = {
+                    'cost': nval+elem,
+                    'prev': ncoor,
+                    'currval': elem,
+                }
+            # ncost = table[ncoor]['cost']
+            newcost = table[curr]['cost']+elem
+            if table[curr]['cost'] > newcost:
+                table[curr] = {
+                    'cost': newcost,
+                    'prev': ncoor,
+                    'currval': elem,
+                }
+    # return table[endcoord]['cost']
+    from pprint import pprint
+    pprint(table)
 
-        if table[coord]['cost'] > cost:
-            table[coord] = {'cost': cost, 'prev': prev}
+def set_cost(curr, elem, ncoor, table):
+    # ncoor = neigh # selected[0] # coors are in '0'
+    ncost = table[ncoor]['cost']
+    cost = elem+ncost
+    if curr in table and table[curr]['cost'] < cost:
+        return # continue
+    table[curr] = {'cost': cost, 'prev': ncoor, 'currval': elem}
 
-        # val = get_val(grid, coord)
-        # print(total_risk, "+", val, '=', total_risk+val)
-        # total_risk += val
-        # if coord == endcoord:
-            # print("found the end!")
-            # q.task_done()
-            # break
-
-        neighs = get_neighbors(grid, coord)
-        for ncoor, ncost in sorted((n for n in neighs if n), key=lambda x: x[1]):
-            print("putting ncoor", ncoor)
-            if ncoor not in table:
-                q.put((ncoor, coord, ncost+cost)) # neighbor coordinates
-            elif ncost+cost < table[ncoor].get('cost', 0):
-                q.put((ncoor, coord, ncost+cost)) # neighbor coordinates
-
-        # print(sns)
-        # m = min(neighs, key=lambda x: x[1])
-        # print(neighs)
-        # print(m)
-        # for n in neighs:
-
-        # q.put(nextco)
-        q.task_done()
-        # for n in nodes:
-    print(table)
-    return cost
-
-def get_neighbors(grid, coord):
+def get_neighbors_reverse(grid, coord):
     coords = (
-            (coord[0]+1, coord[1]), # move right
-            (coord[0], coord[1]+1), # move down
+            (coord[0]-1, coord[1]), # move up
+            (coord[0], coord[1]-1), # move left
         )
     neighs = []
     for newco in coords:
         n = get_coord(grid, newco)
-        if n[1] is None: # 0 could be a valid cost!
+        if n[1] is None or any(i < 0 for i in newco): # 0 could be a valid cost!
             continue
         neighs.append(n)
-    return neighs # first right then down
+    return neighs # first up then left
+
+def get_neighbors(grid, coord):
+    coords = (
+            (coord[0]-1, coord[1]), # move up
+            (coord[0]+1, coord[1]), # move down
+            (coord[0], coord[1]-1), # move left
+            (coord[0], coord[1]+1), # move right
+        )
+    neighs = []
+    for newco in coords:
+        n = get_coord(grid, newco)
+        # if n[1] is None: # 0 could be a valid cost!
+        if n[1] is None or any(i < 0 for i in newco): # 0 could be a valid cost!
+            continue
+        neighs.append(n)
+    return neighs # up, down, left, right
 
 def get_coord(grid, coord):
     try:
@@ -91,8 +109,10 @@ def make_grid(fd):
 def print_grid(grid):
     for r in grid:
         # print("".join(r))
+        # fmt = "{:>2}"*len(r)
+        # print(fmt.format(r))
         for i in r:
-            print(i, end="")
+            print(i, end=" ")
         print()
     print()
 
